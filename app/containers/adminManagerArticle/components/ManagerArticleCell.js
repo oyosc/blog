5a5dbc4e989b028905d0c9005485cdaf91484336
oyosc <chenlei6439186@gmail.com>
2018-07-02 11:53:25 +0800

    update article

--- app/containers/adminManagerArticle/components/ManagerArticleCell.js
+++ app/containers/adminManagerArticle/components/ManagerArticleCell.js
@@ -22,7 +22,6 @@ export const ManagerArticleCell = (props) => {
                 <Button type='primary' icon='edit' onClick={()=>{props.edit_article(props.data._id); props.history.push('/admin/newArticle')}}>编辑</Button>
                 <Button type='primary' icon='delete' onClick={() => props.delete(props.data._id)}>删除</Button>
                 <Button type='primary' icon='eye-o' onClick={() => {props.history.push(`/detail/${props.data._id}`, {id: props.data._id});props.get_article_detail(props.data._id)}}>查看</Button>
-
             </div>
         </div>
     )
