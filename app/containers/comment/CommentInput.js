09a10a29ca31e875fb76f058eac5c59fb745a76b
oyosc <chenlei6439186@gmail.com>
2018-07-05 13:12:53 +0800

    update comment

--- app/containers/comment/CommentInput.js
+++ app/containers/comment/CommentInput.js
@@ -1,9 +1,44 @@
 import React, {Component} from 'react'
+import style from './index.css'
 
 class CommentInput extends Component{
+    constructor(){
+        super()
+        this.state = {
+            username: '',
+            content: ''
+        }
+    }
+
+    handleUserNameChange(event){
+        this.setState({username: event.target.value})
+    }
+
+    handleContentChange(event){
+        this.setState({content: event.target.value})
+    }
+
     render(){
         return (
-            <div>CommentInput</div>
+            <div className={`${style.comment-input}`}>
+                <div className={`${style.comment-field}`}>
+                    <span className={`${style.comment-field-name}`}>用户名: </span>
+                    <div className={`${style.comment-field-input}`}>
+                        <input value={this.state.username} onChange={this.handleUserNameChange.bind(this)}/>
+                    </div>
+                </div>
+                <div className={`${style.comment-field}`}>
+                    <span className={`${style.comment-field-name}`}>评论内容: </span>
+                    <div className={`${comment-field-input}`}>
+                        <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)}/>
+                    </div>
+                </div>
+                <div className='comment-field-button'>
+                    <button>
+                        发布
+                    </button>
+                </div>
+            </div>
         )
     }
 }
