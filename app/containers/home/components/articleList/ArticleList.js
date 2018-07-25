f44075f29b1090732fbba98ef67ea4e43827c490
oyosc <chenlei6439186@gmail.com>
2018-07-25 20:00:47 +0800

    add articleList

---
+++ app/containers/home/components/articleList/ArticleList.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import {ArticleListCell} from '../articleListCell/ArticleListCell'
+
+export default class ArticleList extends Component{
+    constructor(props){
+        super(props)
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+    }
+
+    render(){
+        return(
+            <div>
+                {
+                    this.props.data.map((item, index) => {
+                        <ArticleListCell getArticleDetail={this.props.getArticleDetail} history={this.props.history} key={index} data={item}/>
+                    })
+                }
+            </div>
+        )
+    }
+}
