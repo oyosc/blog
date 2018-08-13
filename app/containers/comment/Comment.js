058ff1cb8456cc07b038652476d05fc96309360b
oyosc <chenlei6439186@gmail.com>
2018-08-13 10:17:35 +0800

    fix comment createdTime

--- app/containers/comment/Comment.js
+++ app/containers/comment/Comment.js
@@ -41,7 +41,7 @@ class Comment extends Component{
         const comment = this.props.comment
         const duration = (+Date.now() - comment.createdTime) /1000
         this.setState({
-            timeString: duration > 60 ? (duration > 3600 ? (duration > 86400 ? (duration > 259200 ? this.toLocalDate(this.state.timeString) : `${Math.round(duration/3600)}天前`) : `${Math.round(duration/3600)}小时前`) : `${Math.round(duration/60)}分钟前`) : `${Math.round(Math.max(duration, 1))} 秒前`
+            timeString: duration > 60 ? (duration > 3600 ? (duration > 86400 ? (duration > 259200 ? this.toLocalDate(comment.createdTime) : `${Math.round(duration/3600)}天前`) : `${Math.round(duration/3600)}小时前`) : `${Math.round(duration/60)}分钟前`) : `${Math.round(Math.max(duration, 1))} 秒前`
         })
     }
 
