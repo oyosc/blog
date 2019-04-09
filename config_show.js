26ba2e006fbc4b2e9690e0725093f3d88d3bdc3e
oyosc <chenlei6439186@gmail.com>
2019-04-09 15:20:12 +0800

    add config_show

---
+++ config_show.js
@@ -0,0 +1,79 @@
+// 添加生产环境或者开发环境下的配置
+let prod = {
+    fore_end_url: 'http://127.0.0.1',
+    api_url: 'http://127.0.0.1:3033',
+    md5_suffix: 'eisdsadawwada这个是加密的信息哦%%%@@!',
+    githubOauth: {
+        clientID: 'xxxx1800fc3ea625exx',
+        clientSecret: 'xxx4b61778532d83301ad3b30e9dbc725008764b',
+        callbackURL: 'http://127.0.0.1/api/auth/github/callback'
+    },
+    corsConfig: {
+        origin: 'http://127.0.0.1'
+    },
+    jwtConfig: {
+        jwt_secret: 'koa_react_secret'
+    },
+    mongoConfig: {
+        ip: '127.0.0.1',
+        port: '27017',
+        db: 'blog'
+    },
+    apiProxy: {
+        ip: '127.0.0.1',
+        port: '8080'
+    },
+    nodeConfig: {
+        ip: '127.0.0.1',
+        port: '3030'
+    },
+    redisConfig: {
+        ip: '127.0.0.1',
+        port: '6379'
+    },
+    issueSize: 30,
+    apiUrl: 'https://api.github.com/repos/oyosc/blog',
+    issueAccessToken: 'xxxxxxxx8dd94b28841c4ca4b9e00735efxxxxxx',
+    issueState: 'open',
+    issueUnfiledFlag: '未归档',
+    issueFiledFlag: '已归档'
+}
+
+let dev = {
+    fore_end_url: 'http://127.0.0.1',
+    api_url: 'http://127.0.0.1:3033',
+    md5_suffix: 'eisdsadawwada这个是加密的信息哦%%%@@!',
+    githubOauth: {
+        clientID: '4c44c1800fc3ea625eb7',
+        clientSecret: '730740923ddff6294cefe4ab719eaaf417ad443d',
+        callbackURL: 'http://127.0.0.1/api/auth/github/callback'
+    },
+    corsConfig: {
+        origin: 'http://127.0.0.1'
+    },
+    jwtConfig: {
+        jwt_secret: 'koa_react_secret'
+    },
+    mongoConfig: {
+        ip: '127.0.0.1',
+        port: '27017',
+        db: 'blog'
+    },
+    apiProxy: {
+        ip: '127.0.0.1',
+        port: '8080'
+    },
+    nodeConfig: {
+        ip: '127.0.0.1',
+        port: '3030'
+    },
+    redisConfig: {
+        ip: '127.0.0.1',
+        port: '6379'
+    }
+}
+
+module.exports = {
+    prod,
+    dev
+}
